b2403852c6cf8d65ab7489dbdb861b4dba40e5f2
sofia25guzman <123517292+sofia25guzman@users.noreply.github.com>
2023-02-20 15:27:02 -0300

    Add files via upload

--- CREACION OBJETOS. PROYECTO FINAL.sql
+++ CREACION OBJETOS. PROYECTO FINAL.sql
@@ -11,7 +11,8 @@ CREATE TABLE if not exists cliente (
     nivel_educacion varchar(20),
     estado_civil varchar(20),
     ingresos numeric
-);
+)
+;
 
 DROP TABLE if exists compra;
 CREATE TABLE if not exists compra ( 
@@ -61,35 +62,44 @@ CREATE TABLE if not exists campaña (
 
 ALTER TABLE cliente
 ADD CONSTRAINT FK_id_producto
-FOREIGN KEY (id_producto) REFERENCES producto(id_producto);
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+ON UPDATE CASCADE
+;
 
 ALTER TABLE compra
 ADD CONSTRAINT FK_id_cliente
-FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+ON UPDATE CASCADE;
 
 ALTER TABLE compra
 ADD CONSTRAINT FK_id_producto_compra
-FOREIGN KEY (id_producto) REFERENCES producto(id_producto);
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+ON UPDATE CASCADE;
 
 ALTER TABLE producto
 ADD CONSTRAINT FK_id_cliente_producto
-FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+ON UPDATE CASCADE;
 
 ALTER TABLE periodo
 ADD CONSTRAINT FK_id_compra_periodo
-FOREIGN KEY (id_compra) REFERENCES compra(id_compra);
+FOREIGN KEY (id_compra) REFERENCES compra(id_compra)
+ON UPDATE CASCADE;
 
 ALTER TABLE quejas
 ADD CONSTRAINT FK_id_producto_quejas
-FOREIGN KEY (id_producto) REFERENCES producto(id_producto);
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+ON UPDATE CASCADE;
 
 ALTER TABLE quejas
 ADD CONSTRAINT FK_id_cliente_quejas
-FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+ON UPDATE CASCADE;
 
 ALTER TABLE supermercado
 ADD CONSTRAINT FK_id_cliente_supermercado
-FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+ON UPDATE CASCADE;
 
 ALTER TABLE supermercado
 ADD CONSTRAINT FK_id_producto_supermercado
@@ -134,7 +144,7 @@ GROUP BY nivel_educacion
 
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS iva_precio;
+DROP FUNCTION IF EXISTS iva_precio$$
 CREATE FUNCTION  iva_precio ( precio DECIMAL(11,2) , IVA DECIMAL (2,2) )
 RETURNS DECIMAL(11,2)
 DETERMINISTIC
@@ -149,7 +159,7 @@ $$
 
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS mail_sucursal;
+DROP FUNCTION IF EXISTS mail_sucursal$$
 CREATE FUNCTION mail_sucursal ( id_sucursal VARCHAR(20), mail VARCHAR(20))
 RETURNS VARCHAR (20)
 DETERMINISTIC
@@ -165,7 +175,7 @@ $$
 -- 1. permite identificar si existe una queja y la relaciona con el producto y al código de sucursal al que pertenece
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS queja_sucursal;
+DROP PROCEDURE IF EXISTS queja_sucursal$$
 CREATE PROCEDURE queja_sucursal ()
 	BEGIN
 		SELECT quejas.existe_queja, quejas.id_producto, supermercado.id_sucursal
@@ -179,7 +189,7 @@ $$
 -- 2. identifica a los peridos con mayor fidelización de clientes, es decir en donde existió menor cantidad de dias entre la primera y la última compra
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS fidelizacion;
+DROP PROCEDURE IF EXISTS fidelizacion$$
 CREATE PROCEDURE fidelizacion ()
 	BEGIN
 		SELECT id_periodo, (SELECT DATEDIFF (dia_ultima_compra,dia_primera_compra)) 
@@ -187,5 +197,118 @@ CREATE PROCEDURE fidelizacion ()
 		FROM periodo
         ORDER BY dias_transcurridos;
 	END
-$$
+	$$
         
+
+-- CREACION DE TRIGGERS
+
+-- 1. Creación de un triggers que alimente una tabla de inspección para verificar la hora exacta en la que se ingresan los registros 
+-- en la tabla productos.
+
+DROP TABLE IF EXISTS inspeccion_producto;
+CREATE TABLE IF NOT EXISTS inspeccion_producto (
+	id_producto varchar(20),
+    id_cliente int,
+    precio decimal (10,2),
+    insertado datetime
+    );
+    
+DROP TRIGGER IF EXISTS tr_after_insert_producto;
+CREATE TRIGGER tr_after_insert_producto
+AFTER INSERT ON producto FOR EACH ROW
+INSERT INTO inspeccion_producto
+VALUES (
+	NEW.id_producto,
+    NEW.id_cliente,
+    NEW.precio,
+    NOW()
+    );
+    
+-- probamos el trigger insertando un nuevo valor en la tabla producto
+
+INSERT INTO producto
+VALUES
+	('0072-8800','5525',2500);
+    
+SELECT * FROM inspeccion_producto;
+
+-- 2.Crearemos un trigger que alimente una tabla de control para el administrador sobre la tabla cliente, para que verifique que usuario realiza 
+-- modificaciones, en que fecha y hora, y pueda observar que campo modificó el usuario con una vista completa de los campos antigüos y nuevos.
+
+DROP TABLE IF EXISTS control_administrador;
+CREATE TABLE IF NOT EXISTS control_administrador (
+	ant_id_cliente int,
+    nvo_id_cliente int,
+    ant_id_producto varchar(20),
+    nvo_id_producto varchar(20),
+    ant_nivel_educacion varchar(20),
+    nvo_nivel_educacion varchar(20),
+    ant_estado_civil varchar(20),
+    nvo_estado_civil varchar(20),
+    ant_ingresos decimal(10,2),
+    nvo_ingresos decimal (10,2),
+    usuario varchar (20),
+    fecha_modificacion datetime
+    );
+    
+DROP TRIGGER IF EXISTS tr_update_before_cliente;
+CREATE TRIGGER tr_update_before_cliente
+BEFORE UPDATE ON cliente
+FOR EACH ROW
+INSERT INTO control_administrador
+VALUES
+	(OLD.id_cliente,
+     NEW.id_cliente,
+     OLD.id_producto,
+	 NEW.id_producto,
+     OLD.nivel_educacion,
+     NEW.nivel_educacion,
+     OLD.estado_civil,
+     NEW.estado_civil,
+     OLD.ingresos,
+     NEW.ingresos,
+     current_user(),
+     now()
+     );
+
+-- probamos el trigger actualizando un valores de la tabla clientes del id_cliente=965
+
+UPDATE cliente 
+SET nivel_educacion='doctorado', ingresos=960000
+WHERE id_cliente='965';
+
+SELECT * FROM control_administrador;
+
+-- 3. Trigger para crear una tabla de resplado de la base de clientes para futuros proyectos, en caso que un usuario elimine uno de los
+-- registros de la tabla clientes.
+
+DROP TABLE IF EXISTS backup_clientes;
+CREATE TABLE IF NOT EXISTS backup_clientes (
+	id_cliente int,
+    id_producto varchar(20),
+    nivel_educacion varchar(20),
+    estado_civil varchar(20),
+    ingresos decimal(10,2),
+    fecha_eliminacion datetime
+    );
+
+DROP TRIGGER IF EXISTS tr_delete_after_clientes;
+CREATE TRIGGER tr_delete_after_clientes
+AFTER DELETE ON cliente
+FOR EACH ROW
+INSERT INTO backup_clientes
+VALUES (
+	old.id_cliente,
+    old.id_producto,
+    old.nivel_educacion,
+    old.estado_civil,
+    old.ingresos,
+    now()
+    );
+
+-- probamos el trigger eliminando registros de la tabla cliente
+
+DELETE FROM cliente
+WHERE id_cliente='4141' OR id_cliente='4855';  
+
+SELECT * FROM backup_clientes;
